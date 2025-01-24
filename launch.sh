241da7fbee283e082e4164a540963c62832661a5
Frede H <fh@manjaro.org>
2025-01-24 14:11:11 +0000

    Consolidating application json data

--- launch.sh
+++ launch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # Script to generate mo files in a temp locale folder
 # Use it only for testing purpose
-export PLUGIN_HELLO=True
-export PYTHONPATH="$HOME/test/gnome-layout-switcher/bin:$HOME/Data/projects/application-utility"
-rm -rf locale
-mkdir locale
-cd po
-for lang in $(ls *.po); do
+export APPS_PROJECT_DIR="/a/projects/repos/manjaro/application-utility"
+export LAYOUT_PROJECT_DIR="/a/projects/repos/manjaro/gnome-layout-switcher/bin"
+rm -rf $PWD/locale
+mkdir $PWD/locale
+for lang in $(ls $PWD/po/*.po); do
     lang=${lang::-3}
-    mkdir -p ../locale/${lang//_/-}/LC_MESSAGES
-    msgfmt -c -o ../locale/${lang//_/-}/LC_MESSAGES/manjaro-hello.mo $lang.po
+    mkdir -p $PWD//locale/${lang//_/-}/LC_MESSAGES
+    msgfmt -c -o $PWD//locale/${lang//_/-}/LC_MESSAGES/manjaro-hello.mo $lang.po
 done
-cd ..
+export PLUGIN_HELLO=True
+export PYTHONPATH="${APPS_PROJECT_DIR}:${LAYOUT_PROJECT_DIR}"
 python3 src/manjaro_hello.py --dev
